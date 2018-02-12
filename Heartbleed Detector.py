b9ebb8f7952aa4dcaa6ecab4d521f538e3a9c0e1
jamiecoupe <Jamie.coupe@sky.com>
2018-02-12 16:29:43 +0000

    The plan directing the stages of the script to be created

---
+++ Heartbleed Detector.py
@@ -0,0 +1,20 @@
+"""This script is designed to intercept SSL
+packets coming into the device and to warn
+the user if the Heartbleed bug is being exploited."""
+
+"""This script is for educational use only and is 
+property of Jamie Coupe"""
+
+#Import Packet Capturing module
+
+#Link to GITHUB
+
+#Intercept Network Traffic
+
+#Filter SSL Packets
+    #Delete Non-SSL Packets
+
+#Compare Size and Length Fields
+    #Repeat if packet is not malicious
+
+    #If Malicious packet > Alert/Drop connection

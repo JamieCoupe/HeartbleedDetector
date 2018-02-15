6a46f9215889c7d9f59bb2d61ab952cf54ea0e9c
jamiecoupe <Jamie.coupe@sky.com>
2018-02-15 16:08:36 +0000

    Fixed plan and added a function that filters ARP packets as an example

--- Heartbleed Detector.py
+++ Heartbleed Detector.py
@@ -4,16 +4,23 @@ the user if the Heartbleed bug is being exploited.This script is for educational
 property of Jamie Coupe"""
 
 # Import Socket module
-import scapy.all
+from scapy.all import *
+from scapy.layers.ssl_tls import *
+
+#Function for checking fields
+def field_comparison(pkt):
+    if pkt[ARP].op == 1:  # who-has (request)
+        return 'Request: {} is asking about {}'.format(pkt[ARP].psrc, pkt[ARP].pdst)
+    if pkt[ARP].op == 2:  # is-at (response)
+        return '*Response: {} has address {}'.format(pkt[ARP].hwsrc, pkt[ARP].psrc)
 
 # Intercept Network Traffic
+pkt = sniff(prn=field_comparison , count=10)
 
 
-# Filter SSL Packets
-    # Delete Non-SSL Packets
 
-    # Compare Size and Length Fields
-        # Repeat if packet is not malicious
+# Compare Size and Length Fields
+# Repeat if packet is not malicious
 
-
-        # If Malicious packet > Alert/Drop connection
+# If Malicious packet > Alert/Drop connection
+#This can be done with the stop_filter in sniff function. need to define function

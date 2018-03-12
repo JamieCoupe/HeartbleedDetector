eba2715adc0c51787981acde2ce2639efc7dfcb8
jamiecoupe <Jamie.coupe@sky.com>
2018-03-12 20:00:30 +0000

    Added functions and got it working

--- Heartbleed_Detector.py
+++ Heartbleed_Detector.py
@@ -5,32 +5,70 @@ property of Jamie Coupe"""
 
 # Import scapy and scapy ssl-tls layers
 from scapy.all import *
-from scapy.layers.ssl_tls import *
+from scapy.layers.scapy_ssl_tls.ssl_tls import *
+
+
+# Function to check if message is a heartbeat message
+def is_heartbeat(p):
+    if p[SSL][TLSRecord].content_type == 24:
+        return True
+
+    else:
+        return False
+
+
+# Function to get length of heartbeat message
+def get_length(p):
+
+    length = p[SSL][TLSRecord].length
+    return length
+
+
+# Function to get actual length of heartbeat message
+def get_actual_length(p):
+
+    # Set data field to data value
+    raw_hex = [pkt[2][SSL][TLSRecord]]
+    data = str(raw_hex)
+    processed_data = data[92:94] + data[96:98]
+    return int("0x" + processed_data, 0)
+
 
 # Function for checking fields
-"""def field_comparison(pkt):
+def field_comparison(p):
 
-    if pkt[]: 
-        return pkt[]"""
+    if is_heartbeat(p):
 
-# Ask user how many packets they want
-pktC = int(raw_input("Please enter how many packets you wish to capture:"))
+        if get_length(p) != get_actual_length(p):
+            print "This packet is possibly malicious, now dropping connection"
+            return 1
+
+        else:
+            print "The packet is not malicious"
+            return 0
+
 
 # Intercept Network Traffic
-pkt = sniff(lfilter=lambda x: x.haslayer(TLSHeartbeatMessageType), count=pktC)
+pkt = sniff(iface="eth1", lfilter=lambda x: x.haslayer(TLSRecord), stop_filter=field_comparison)
 
-# Display the sniffed packets
-pktI = 0
 
-while pktI < pktC:
+# Display all the sniffed packets
+"""i = 0
 
+while i < 3:
     # Print the packets
-        print pkt[pktI].summary()
-        print pktI
-        pktI += 1
+    print "<<New Packet>>"
+    print "Length = " + str(pkt[i][SSL][TLSRecord].length)
+    print pkt[i][SSL][TLSRecord].show()
 
-# Compare Size and Length Fields
-# Repeat if packet is not malicious
+    i += 1"""
+
+# Display the heartbeat request
+"""print "<<Heartbeat Request>>"
+print get_length(pkt[2])
+print get_actual_length(pkt[2])"""
+
+field_comparison(pkt[2])
 
 # If Malicious packet > Alert/Drop connection
 # This can be done with the stop_filter in sniff function. need to define function
--- Scapy_Test.py
+++ Scapy_Test.py
@@ -5,26 +5,26 @@ property of Jamie Coupe"""
 
 # Import scapy and scapy ssl-tls layers
 from scapy.all import *
-from scapy.layers.ssl_tls import *
+from scapy_ssl_tls import *
 
 # Function for checking fields
 def field_comparison(pkt):
-
-    if pkt[]: 
-        return pkt[]
+    if pkt.op == "who-has":
+        return True
+    else:
+        return False
 
 # Ask user how many packets they want
 pktC = int(raw_input("Please enter how many packets you wish to capture:"))
 
 # Intercept Network Traffic
-pkt = sniff(lfilter=lambda x: x.haslayer(ARP), count=pktC)
+pkt = sniff(lfilter=ARP, count=pktC)
 
 # Display the sniffed packets
 pktI = 0
 
 while pktI < pktC:
-
     # Print the packets
-        print pkt[pktI].summary()
-        print pktI
-        pktI += 1
+    print pkt[pktI].summary()
+    print pktI
+    pktI += 1
